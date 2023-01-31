2e107fddced86643f0407f384d65e75272ffc917
unknown <imamovic.kerim@gmail.com>
2023-01-31 15:31:20 +0100

    izmjene 2
    -izmjene u vite.confing.js fajlu
    -izmjene u README.md fajlu
    -izmjene u package.json fajlu

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ![reactjs-vitejs-tailwindcss-portfolio-cc-2023](portfolio.jpg)
+
+
+How to Deploy Your Vite App to Github Pages
+https://www.youtube.com/watch?v=yo2bMGnIKE8
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/reactjs-vitejs-tailwindcss-portfolio-cc-2023',
   plugins: [react()],
 });
